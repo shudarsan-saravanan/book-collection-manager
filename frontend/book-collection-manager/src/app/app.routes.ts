13af38f87c3d9368825d0328ac9f67f853ee2474
shudarsan-saravanan <shudarsansaravanan@gmail.com>
2024-11-05 17:04:06 +0100

    Added components and routing

--- frontend/book-collection-manager/src/app/app.routes.ts
+++ frontend/book-collection-manager/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BookFormComponent },
+  { path: 'list', component: BookListComponent },
+];
