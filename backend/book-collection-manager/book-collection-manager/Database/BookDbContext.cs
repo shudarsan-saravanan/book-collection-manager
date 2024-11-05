bdb5cbc5bbcbfedabb302b2f59e6c4f590f49a0e
shudarsan-saravanan <shudarsansaravanan@gmail.com>
2024-11-05 14:57:31 +0100

    Adding database

---
+++ backend/book-collection-manager/book-collection-manager/Database/BookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using book_collection_manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace book_collection_manager.Database
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
