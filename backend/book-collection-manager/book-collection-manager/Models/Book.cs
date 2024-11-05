89ec981edab5bbcb19cc32ea52a926663ae2cb8f
shudarsan-saravanan <shudarsansaravanan@gmail.com>
2024-11-05 14:57:16 +0100

    Defining models

---
+++ backend/book-collection-manager/book-collection-manager/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace book_collection_manager.Models
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
